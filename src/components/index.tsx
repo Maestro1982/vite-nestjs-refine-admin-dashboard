fb2c6f57f368f3ee487a10bbc72ff8dbd0ee6225
Maestro1982 <kris.tote@telenet.be>
2024-01-23 15:36:37 +0100

    modify package.json, index.tsx in src pages home folder, create index.tsx in src components folder, deals-chart.tsx, total-count-card.tsx and upcoming-events.tsx in src components home folder, accordion-header.tsx, kanban.tsx, lastest-activities.tsx, project-card.tsx and upcoming-events.tsx in src components skeleton folder, index.tsx in src constants folder

---
+++ src/components/index.tsx
@@ -0,0 +1,20 @@
+import DashboardTotalCountCard from './home/total-count-card';
+import DealsChart from './home/deals-chart';
+import UpcomingEvents from './home/upcoming-events';
+
+import AccordionHeaderSkeleton from './skeleton/accordion-header';
+import KanbanColumnSkeleton from './skeleton/kanban';
+import LatestActivitiesSkeleton from './skeleton/latest-activities';
+import ProjectCardSkeleton from './skeleton/project-card';
+import UpcomingEventsSkeleton from './skeleton/upcoming-events';
+
+export {
+  DashboardTotalCountCard,
+  DealsChart,
+  UpcomingEvents,
+  AccordionHeaderSkeleton,
+  KanbanColumnSkeleton,
+  LatestActivitiesSkeleton,
+  ProjectCardSkeleton,
+  UpcomingEventsSkeleton,
+};
